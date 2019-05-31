179da1cc8a16baa3aa624a749e9434f042b8e67f
nhattranorient <nhat.tran@orientsoftware.com>
2019-05-31 09:57:10 +0700

    Add files

---
+++ src/RFQ/RFQ.Data/Migrations/20190531024740_addFilepath.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RFQ.Data.Migrations
+{
+    public partial class addFilepath : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DocumentPath",
+                table: "Applicant",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DocumentPath",
+                table: "Applicant");
+        }
+    }
+}
